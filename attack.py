74b324e1dfa27a534fdef92a99e1510b517c56be
Sherry Wong <sherryw@royal-12.cs.wisc.edu>
2023-03-03 16:38:17 -0600

    Sherry struggle 1

--- attack.py
+++ attack.py
@@ -5,10 +5,17 @@
 # usage: python3 attack.py ciphertext
 # Outputs a modified ciphertext and tag
 
+debug = 0
+debug2 = 0
+debug3 = 0
+
 import sys
+import array
 
 # Grab ciphertext from first argument
 ciphertextWithTag = bytes.fromhex(sys.argv[1])
+mutableFullCiphertext = array.array('u', sys.argv[1])
+#mutableFullCiphertext = bytearray(sys.argv[1], "utf-8")
 
 if len(ciphertextWithTag) < 16+16+32:
   print("Ciphertext is too short!")
@@ -22,4 +29,26 @@ tag = ciphertextWithTag[len(ciphertextWithTag)-32:]
 
 # TODO: Print the new encrypted message
 # you can change the print content if necessary
-print(ciphertext.hex() + tag.hex())
+if(debug):
+  print("iv.hex() = " + str(iv.hex()))
+  print()
+  print("ciphertext.hex() = " + str(ciphertext.hex()))
+  print()
+  print("tag.hex() = " + str(tag.hex()))
+  print()
+
+if(debug2): print("len(mutableFullCiphertext) = " + str(len(mutableFullCiphertext)))
+
+modified_msg = mutableFullCiphertext
+if(debug3):print(len(modified_msg))
+if(debug3):print(modified_msg[100])
+modified_msg[100] = '0'
+#for i in range(100, 110):
+#  modified_msg[i] = '0'
+if(debug3):print(modified_msg[100])
+if(debug2): print("ciphertextWithTag.hex() = \n" + str(ciphertextWithTag.hex()))
+if(debug2): print()
+if(debug2): print("modified_msg = ")
+#if(debug2): print("modified_msg = \n" +str(modified_msg)[12:len(str(modified_msg))-2])
+print(str(modified_msg)[12:len(str(modified_msg))-2])
+#print(str(tag.hex()) + str(modified_msg)[12+len(str(tag.hex())):len(str(modified_msg))-2])
